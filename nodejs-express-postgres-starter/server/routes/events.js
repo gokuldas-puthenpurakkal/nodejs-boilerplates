a12f996727c630aa70768667ec9488ec53334a63
gokuldas-puthenpurakkal <gokuldas.puthenpurakkal@gmail.com>
2017-10-26 21:21:39 +0530

    removed all files

--- nodejs-express-postgres-starter/server/routes/events.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    const eventsController = app.controllers.events;
-    app.routes.get('/events', eventsController.getAvailableEvents);
-    app.routes.post('/events', eventsController.addEvent);
-    app.routes.put('/events/:eventId', eventsController.updateEvent);
-    app.routes.delete('/events/:eventId', eventsController.deleteEvent);
-};
